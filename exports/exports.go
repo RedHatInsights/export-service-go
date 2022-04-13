d6db4cea908ca78fd8618e87f07d966274e7227e
Michael Skarbek <mskarbek@redhat.com>
2022-04-13 09:08:26 -0400

    model updates

--- exports/exports.go
+++ exports/exports.go
@@ -3,8 +3,10 @@ package exports
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
 
 	"github.com/maskarb/export-service-go/db"
 	"github.com/maskarb/export-service-go/logging"
@@ -12,6 +14,16 @@ import (
 )
 
 var log = logging.Log
+
+type APIExport struct {
+	ID          string     `json:"id"`
+	Name        string     `json:"name"`
+	CreatedAt   time.Time  `json:"created"`
+	CompletedAt *time.Time `json:"completed,omitempty"`
+	Expires     *time.Time `json:"expires,omitempty"`
+	Format      string     `json:"format"`
+	Status      string     `json:"status"`
+}
 
 type IError struct {
 	Field string
@@ -35,35 +47,47 @@ func PostExport(w http.ResponseWriter, r *http.Request) {
 		JSONError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	// var errors []*IError
-	// if err := payload.Validate(); err != nil {
-	// 	if e, ok := err.(validation.Errors); ok {
-	// 		for k, v := range e {
-	// 			var el IError
-	// 			el.Field = k
-	// 			el.Msg = v.Error()
-	// 			errors = append(errors, &el)
-	// 		}
-	// 	}
-	// 	JSONError(w, errors, http.StatusBadRequest)
-	// 	return
-	// }
-
 	tx := db.DB.Create(&payload)
 	if tx.Error != nil {
 		log.Error(tx.Error)
 	}
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(&payload); err != nil {
+		log.Error("Error while trying to encode")
+	}
 
 }
 
-func ListExports(w http.ResponseWriter, r *http.Request) {}
+func ListExports(w http.ResponseWriter, r *http.Request) {
+	exports := []*APIExport{}
+	result := db.DB.Model(&models.ExportPayload{}).Find(&exports)
+	if result.Error != nil {
+		JSONError(w, result.Error, http.StatusBadRequest)
+		return
+	}
+	if err := render.RenderList(w, r, NewExportListResponse(exports)); err != nil {
+		JSONError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
 
 func GetExport(w http.ResponseWriter, r *http.Request) {}
 
 func DeleteExport(w http.ResponseWriter, r *http.Request) {}
 
 func GetExportStatus(w http.ResponseWriter, r *http.Request) {}
+
+func (e *APIExport) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func NewExportListResponse(exports []*APIExport) []render.Renderer {
+	list := []render.Renderer{}
+	for _, export := range exports {
+		list = append(list, export)
+	}
+	return list
+}
 
 func JSONError(w http.ResponseWriter, err interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
--- models/models.go
+++ models/models.go
@@ -8,21 +8,48 @@ import (
 	"gorm.io/gorm"
 )
 
+type PayloadFormat string
+
+const (
+	CSV  PayloadFormat = "csv"
+	JSON PayloadFormat = "json"
+)
+
+type PayloadStatus string
+
+const (
+	Pending  PayloadStatus = "pending"
+	Running  PayloadStatus = "running"
+	Complete PayloadStatus = "complete"
+)
+
 type ExportPayload struct {
-	ID             string         `gorm:"type:uuid;primarykey" json:"id"`
-	CreatedAt      time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt      time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
-	Expires        *time.Time     `json:"expires,omitempty"`
-	Name           string         `json:"name"`
-	Application    string         `json:"application"`
-	Format         string         `json:"format"`
-	Resources      datatypes.JSON `json:"resources"`
-	AccountID      string         `json:"account_id"`
-	OrganizationID string         `json:"organization_id"`
-	Username       string         `json:"username"`
+	ID             string        `gorm:"type:uuid;primarykey" json:"id"`
+	CreatedAt      time.Time     `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt      time.Time     `gorm:"autoUpdateTime" json:"updated_at"`
+	CompletedAt    *time.Time    `json:"completed_at,omitempty"`
+	Expires        *time.Time    `json:"expires,omitempty"`
+	Name           string        `json:"name"`
+	Application    string        `json:"application"`
+	Format         PayloadFormat `gorm:"type:string" json:"format"`
+	Status         PayloadStatus `gorm:"type:string" json:"status"`
+	Sources        []*Source     `gorm:"type:json" json:"sources"`
+	AccountID      string        `json:"-"`
+	OrganizationID string        `json:"-"`
+	Username       string        `json:"-"`
+}
+
+type Source struct {
+	ID       string         `json:"id"`
+	Resource string         `json:"resource"`
+	Filters  datatypes.JSON `json:"filters"`
 }
 
 func (ep *ExportPayload) BeforeCreate(tx *gorm.DB) error {
 	ep.ID = uuid.NewString()
+	ep.Status = Pending
+	for _, source := range ep.Sources {
+		source.ID = uuid.NewString()
+	}
 	return nil
 }

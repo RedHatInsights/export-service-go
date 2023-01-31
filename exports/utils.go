0fda7ea66147fb78e15c8810ec393ca4cbe0e049
Justin Orringer <justinorringer@gmail.com>
2023-01-31 13:57:09 -0500

    Removed debug return

--- exports/utils.go
+++ exports/utils.go
@@ -21,7 +21,7 @@ func initQuery(q url.Values) (result models.QueryParams, err error) {
 	// created and expires should be date only, not date-time strings
 	format := "2006-01-02"
 	if created != "" {
-		result.Created, err = time.Parse(format, created)
+		result.Created, err = time.Parse(time.RFC822, created)
 		if err != nil {
 			return models.QueryParams{}, fmt.Errorf("'%s' is not a valid date-time", created)
 		}
@@ -34,5 +34,5 @@ func initQuery(q url.Values) (result models.QueryParams, err error) {
 		}
 	}
 
-	return models.QueryParams{}, fmt.Errorf("'%s' is not a valid date-time", created)
+	return
 }

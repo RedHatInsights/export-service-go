4e11a57f26ff246be97a7935410c8d9b014be73c
Victor M <vmugicag@redhat.com>
2023-02-21 15:17:24 +0100

    Enable smoke tests on pr_check.sh
    - Amend Quay image name

--- pr_check.sh
+++ pr_check.sh
@@ -3,12 +3,13 @@
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-APP_NAME="export"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="export-service-go"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
-IMAGE="quay.io/cloudservices/export-service-go"  
+APP_NAME="export-service"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="$APP_NAME"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/export-service"
 
-IQE_PLUGINS="export"
-IQE_MARKER_EXPRESSION="smoke"
+IQE_PLUGINS="export_service"
+#IQE_IMAGE_TAG="export-service-7b4b1d0c"
+#IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 IQE_CJI_TIMEOUT="30m"
 
@@ -16,15 +17,11 @@ IQE_CJI_TIMEOUT="30m"
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
+mkdir -p $WORKSPACE/artifacts
+
 source $CICD_ROOT/build.sh
 # source $APP_ROOT/unit_test.sh
-source $CICD_ROOT/deploy_ephemeral_env.sh
-# source $CICD_ROOT/cji_smoke_test.sh
+source "${CICD_ROOT}/deploy_ephemeral_env.sh"
+source "${CICD_ROOT}/cji_smoke_test.sh"
+source "${CICD_ROOT}/post_test_results.sh"
 
-
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF

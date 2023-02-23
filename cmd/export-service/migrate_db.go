e0de5cd0d2a100aacb775af082760214c5ffbda1
dehort <dehort@redhat.com>
2023-02-23 11:10:41 -0600

    Wire up migrations for the embedded database that is used in the tests (#53)

--- cmd/export-service/migrate_db.go
+++ cmd/export-service/migrate_db.go
@@ -1,69 +1,19 @@
 package main
 
 import (
-	"errors"
-
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
-
 	"github.com/redhatinsights/export-service-go/config"
 	"github.com/redhatinsights/export-service-go/db"
 
 	"go.uber.org/zap"
 )
 
-type loggerWrapper struct {
-	*zap.SugaredLogger
-}
-
-func (lw loggerWrapper) Verbose() bool {
-	return true
-}
-
-func (lw loggerWrapper) Printf(format string, v ...interface{}) {
-	lw.Infof(format, v...)
-}
-
 func performDbMigration(cfg *config.ExportConfig, log *zap.SugaredLogger, direction string) error {
 
-	log.Info("Starting Export Service DB migration")
-
-	db, err := db.OpenPostgresDB(*cfg)
+	databaseConn, err := db.OpenPostgresDB(*cfg)
 	if err != nil {
 		log.Error("Unable to initialize database connection", "error", err)
 		return err
 	}
 
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	if err != nil {
-		log.Error("Unable to get postgres driver from database connection", "error", err)
-		return err
-	}
-
-	m, err := migrate.NewWithDatabaseInstance("file://db/migrations", "postgres", driver)
-	if err != nil {
-		log.Error("Unable to intialize database migration util", "error", err)
-		return err
-	}
-
-	m.Log = loggerWrapper{log}
-
-	if direction == "up" {
-		err = m.Up()
-	} else if direction == "down" {
-		err = m.Steps(-1)
-	} else {
-		return errors.New("Invalid operation")
-	}
-
-	if errors.Is(err, migrate.ErrNoChange) {
-		log.Info("DB migration resulted in no changes")
-	} else if err != nil {
-		log.Error("DB migration resulted in an error", "error", err)
-		return err
-	}
-
-	return nil
+	return db.PerformDbMigration(databaseConn, log, "file://db/migrations", direction)
 }
---
+++ db/migrate_db.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+
+	"go.uber.org/zap"
+)
+
+type loggerWrapper struct {
+	*zap.SugaredLogger
+}
+
+func (lw loggerWrapper) Verbose() bool {
+	return true
+}
+
+func (lw loggerWrapper) Printf(format string, v ...interface{}) {
+	lw.Infof(format, v...)
+}
+
+func PerformDbMigration(databaseConn *sql.DB, log *zap.SugaredLogger, pathToMigrationFiles string, direction string) error {
+
+	log.Info("Starting Export Service DB migration")
+
+	driver, err := postgres.WithInstance(databaseConn, &postgres.Config{})
+	if err != nil {
+		log.Error("Unable to get postgres driver from database connection", "error", err)
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(pathToMigrationFiles, "postgres", driver)
+	if err != nil {
+		log.Error("Unable to intialize database migration util", "error", err)
+		return err
+	}
+
+	m.Log = loggerWrapper{log}
+
+	if direction == "up" {
+		err = m.Up()
+	} else if direction == "down" {
+		err = m.Steps(-1)
+	} else {
+		return errors.New("Invalid operation")
+	}
+
+	if errors.Is(err, migrate.ErrNoChange) {
+		log.Info("DB migration resulted in no changes")
+	} else if err != nil {
+		log.Error("DB migration resulted in an error", "error", err)
+		return err
+	}
+
+	return nil
+}

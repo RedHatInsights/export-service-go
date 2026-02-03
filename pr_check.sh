6aa447878dd58605c2b8b3c78db0e358eab0a995
Brandon Schneider <github@bschneider.email>
2026-02-03 15:29:05 -0600

    RHCLOUD-44487: Make RBAC an optional dependency (#331)

--- pr_check.sh
+++ pr_check.sh
@@ -13,6 +13,7 @@ IQE_PLUGINS="export_service"
 IQE_FILTER_EXPRESSION=""
 IQE_CJI_TIMEOUT="30m"
 IQE_ENV="clowder_smoke"
+IQE_ENV_VARS="DYNACONF_USER_PROVIDER__rbac_enabled=false"
 EXTRA_DEPLOY_ARGS="--set-parameter export-service/MAX_PAYLOAD_SIZE=20"
 
 # Install bonfire repo/initialize
